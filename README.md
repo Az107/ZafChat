98bb901733228a3447664d4ac2a09effb0844730
Az107 <albertzruiz1@gmail.com>
2020-12-21 23:48:26 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ZafChat
+# ZafChat
---
+++ index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/web/index.html");
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on("msg", (msg) => {
+    io.emit("msg",msg);
+  });
+});
+
+
+
+http.listen(80, () => {
+  console.log('listening on *:3000');
+});
+
